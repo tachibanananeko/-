de0e1f19ae0f120e87208284e00fa63646b139df
tachibanananeko <60609976+tachibanananeko@users.noreply.github.com>
2021-12-16 15:07:47 +0800

    Add files via upload

---
+++ frpc进程维护.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set Program="frpc.exe"  
+tasklist -v | findstr %Program% > NUL  
+if ErrorLevel 1 (  
+  echo %date% %time%  "frpc进程丢失，重启frp服务" >> C:\frp\frp_log\log.txt  
+  start /d "C:\frp\frp_0.36.2_windows_386" frpcqd.bat
+)
+if ErrorLevel 0(
+  echo "frp服务正在运行"
+)  
